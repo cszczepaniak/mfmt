8162e142704620b63626ebddd2ffae6f4b1693f9
cszczepaniak <cszczepaniak@gmail.com>
2019-10-20 21:30:52 -0500

    add scanner and some tests

---
+++ scanner/scanner.go
@@ -0,0 +1,36 @@
+package scanner
+
+// Scanner stores state
+type Scanner struct {
+	source  []rune
+	ch      rune
+	start   int
+	current int
+}
+
+// Scanner needs to keep track of its current position in the file,
+// and should also define functions used to scan various token types in
+// a MATLAB source file.
+
+// NewScanner instantiates a scanner
+func NewScanner(source string) *Scanner {
+	var scanner Scanner
+	scanner.source = []rune(source)
+	scanner.start = 0
+	scanner.ch = scanner.source[0]
+	return &scanner
+}
+
+// next advances the file position
+func (s *Scanner) next() {
+	if s.current < len(s.source)-1 {
+		s.current++
+		s.ch = s.source[s.current]
+	}
+}
+
+func (s *Scanner) skipWhitespace() {
+	for s.ch == ' ' || s.ch == '\n' || s.ch == '\t' || s.ch == '\r' {
+		s.next()
+	}
+}
---
+++ scanner/scanner_test.go
@@ -0,0 +1,27 @@
+package scanner
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScanner_next(t *testing.T) {
+	tests := []struct {
+		name   string
+		source string
+		s      *Scanner
+	}{
+		{
+			name:   "test next",
+			source: "abcdefghi",
+			s:      NewScanner("abcdefghi"),
+		},
+	}
+	for _, tt := range tests {
+		for _, c := range tt.source {
+			assert.Equal(t, c, tt.s.ch)
+			tt.s.next()
+		}
+	}
+}
